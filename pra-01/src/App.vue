79700bf16ed41029b962df76014dafe4ade01fd6
smit312 <smit.coder31@gmail.com>
2022-03-28 15:47:34 +0530

    TE5-T637-practical-01: completed

---
+++ pra-01/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <GalleryCard />
+  </div>
+</template>
+
+<script>
+import GalleryCard from "./components/GalleryCard.vue";
+export default {
+  name: "App",
+  components: {
+    GalleryCard,
+  },
+};
+</script>
+
+

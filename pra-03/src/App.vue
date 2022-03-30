c78e6ba8679abada4f45b21e7cc40a0fdc1d2fcc
smit312 <smit.coder31@gmail.com>
2022-03-30 16:31:00 +0530

    added component: gallerycard, homecomponent, navbar

--- pra-03/src/App.vue
+++ pra-03/src/App.vue
@@ -1,27 +1,15 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div style="background-color: #141c2f; color: white">
+    <Homecomponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Homecomponent from "./components/HomeComponent.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Homecomponent,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ pra-03/src/components/GalleryCard.vue
@@ -0,0 +1,66 @@
+<script>
+export default {
+  name: "GalleryCard",
+  props: ["carTitle", "carImage", "cardetails", "price"],
+};
+</script>
+
+<template>
+  <div
+    class="col-md-3 d-flex justify-between-center user-select-none"
+    style="margin: 0"
+  >
+    <b-card
+      class="mt-5 shadow p-3 mb-3 rounded position-relative"
+      header-tag="header"
+      style="background-color: #1f2a48"
+      align="center"
+      :title="carTitle"
+    >
+      <b-img
+        style="width: auto; height: 10rem"
+        :src="carImage"
+        alt="Fluid-grow image"
+      ></b-img>
+      <b-card-title class="text-center"></b-card-title>
+      <b-card-text class="mt-4 mb-5">{{ cardetails }}</b-card-text>
+      <b-button
+        style="background-color: #1f2a48"
+        class="
+          shadow-lg
+          border-none
+          position-absolute
+          bottom-0
+          left-50
+          mb-3
+          button-center
+        "
+        @click="$emit('CarPrice', price)"
+        v-if="price != null"
+      >
+        Info
+      </b-button>
+      <b-button
+        style="background-color: #1f2a48"
+        class="
+          shadow-lg
+          border-none
+          position-absolute
+          bottom-0
+          left-50
+          mb-3
+          button-center
+        "
+        disabled
+        v-else
+      >
+        available soon...
+      </b-button>
+    </b-card>
+  </div>
+</template>
+<style>
+.button-center {
+  transform: translateX(-50%);
+}
+</style>

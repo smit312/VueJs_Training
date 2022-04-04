7b83323f26df8cc6bfc64b7e73e75b044f2fafce
Smit3112 <smit.t@simformsolutions.com>
2022-04-04 15:32:58 +0530

    added: modal and validation

--- pra-03/src/components/HomeComponent.vue
+++ pra-03/src/components/HomeComponent.vue
@@ -14,6 +14,96 @@
         :price="car.price"
         @CarPrice="CarPrice"
       />
+      <b-button v-b-modal.modal-prevent-closing>Add Car</b-button>
+      <!-- start modal -->
+      <b-modal
+        id="modal-prevent-closing"
+        ref="modal"
+        title="Enter car Detail"
+        @show="resetModal"
+        @hidden="resetModal"
+        @ok="handleOk"
+      >
+        <ValidationObserver ref="observer">
+          <b-form
+            ref="form"
+            slot-scope="{ validate }"
+            @submit.prevent="validate().then(handleSubmit)"
+          >
+            <ValidationProvider rules="required|alpha" name="carName">
+              <b-form-group
+                label="CarName"
+                slot-scope="{ valid, errors }"
+                label-for="name-input"
+                :state="nameState"
+              >
+                <b-form-input
+                  id="name-input"
+                  v-model="CarName"
+                  type="text"
+                  :state="errors[0] ? false : valid ? true : null"
+                  placeholder="Enter car name"
+                ></b-form-input>
+              </b-form-group>
+            </ValidationProvider>
+            <!-- ------------------------------ -->
+            <ValidationProvider
+              rules="required|min:3|max:120 "
+              name="carDetails"
+            >
+              <b-form-group
+                label="CarDetails"
+                label-for="detail-input"
+                invalid-feedback="Details between 3 to 130 character"
+                :state="nameState"
+              >
+                <b-form-textarea
+                  id="detail-input"
+                  v-model="CarDetails"
+                  :state="nameState"
+                  maxlength="120"
+                  minlength="30"
+                  required
+                ></b-form-textarea>
+              </b-form-group>
+            </ValidationProvider>
+            <!-- ------------------------------ -->
+            <ValidationProvider rules="required|" name="carImgURL">
+              <b-form-group
+                label="ImageURL"
+                label-for="url-input"
+                invalid-feedback="Must be url"
+                :state="nameState"
+              >
+                <b-form-input
+                  id="url-input"
+                  v-model="ImageURL"
+                  :state="nameState"
+                  type="url"
+                  required
+                ></b-form-input>
+              </b-form-group>
+            </ValidationProvider>
+            <!-- ------------------------------ -->
+            <ValidationProvider rules="required|integer" name="carPrice">
+              <b-form-group
+                label="Price"
+                label-for="Price"
+                invalid-feedback="Name is required"
+                :state="nameState"
+              >
+                <b-form-input
+                  id="Price"
+                  v-model="Price"
+                  :state="nameState"
+                  required
+                ></b-form-input>
+              </b-form-group>
+            </ValidationProvider>
+          </b-form>
+        </ValidationObserver>
+      </b-modal>
+      <!-- end modal -->
     </div>
   </div>
 </template>
@@ -31,11 +121,42 @@ export default {
   data() {
     return {
       cars: CarData,
+      name: "",
+      nameState: null,
+
+      submittedNames: [],
     };
   },
   methods: {
     CarPrice(price) {
       alert(`Car Price : ${price}`);
+    },
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity();
+      this.nameState = valid;
+      return valid;
+    },
+    resetModal() {
+      this.name = "";
+      this.nameState = null;
+    },
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault();
+      // Trigger submit handler
+      this.handleSubmit();
+    },
+    handleSubmit() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidity()) {
+        return;
+      }
+      // Push the name to submitted names
+      this.submittedNames.push(this.name);
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$bvModal.hide("modal-prevent-closing");
+      });
     },
   },
 };
--- pra-03/src/main.js
+++ pra-03/src/main.js
@@ -6,8 +6,24 @@ Vue.config.productionTip = false;
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  localize,
+} from "vee-validate";
+import en from "vee-validate/dist/locale/en.json";
+import * as rules from "vee-validate/dist/rules";
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Object.keys(rules).forEach((rule) => {
+  extend(rule, rules[rule]);
+});
+
+localize("en", en);
+
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
 
 new Vue({
   render: (h) => h(App),

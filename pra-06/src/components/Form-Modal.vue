4745b0a77c9516c5d8aa381dabcfd9b0d02d3890
Smit3112 <smit.t@simformsolutions.com>
2022-04-27 10:53:06 +0530

    initial commit

---
+++ pra-06/src/components/Form-Modal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Enter car Detail"
+      hide-footer="true"
+      @hide="onHideHandler"
+    >
+      <CarForm
+        modalId="modal-prevent-closing"
+        :formData="$props"
+        v-on="$listeners"
+      />
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import CarForm from "./CarForm.vue";
+
+export default {
+  components: { CarForm },
+  props: ["formData"],
+  data() {
+    return {
+      name: "FormModal",
+    };
+  },
+  methods: {
+    onHideHandler() {
+      this.$parent.selectedCardData = {
+        carId: "",
+        carTitle: "",
+        cardetails: "",
+        carPrice: "",
+        carImage: "",
+      };
+    },
+  },
+};
+</script>
---
+++ pra-06/src/views/carDetails.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="d-flex justify-content-center mt-5">
+      <div>
+        <AlertBox
+          v-if="this.isLoading === false && this.errmsg !== ''"
+          showAlert="true"
+          alertVariant="danger"
+          :alertMessage="this.errmsg"
+        />
+      </div>
+      <b-card
+        :title="this.carData.name"
+        :img-src="this.carData.image"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 30rem"
+        class="mb-2"
+      >
+        <b-card-text>
+          {{ this.carData.details }}
+        </b-card-text>
+        <b-card-text> price : {{ this.carData.price }} </b-card-text>
+
+        <b-button to="/" variant="primary">Back to homepage</b-button>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import AlertBox from "../components/AlertBox.vue";
+export default {
+  name: "CarDetailsPage",
+  components: { AlertBox },
+  data() {
+    return {
+      carData: {
+        image: "",
+        name: "",
+        details: "",
+        price: "",
+      },
+      isLoading: false,
+      errmsg: "",
+    };
+  },
+  methods: {
+    async getCarData(id) {
+      this.isLoading = true;
+      await axios
+        .get(`https://testapi.io/api/dartya/resource/cardata/${id}`)
+        .then((res) => {
+          this.isLoading = false;
+          if (res && res.data) {
+            this.carData = {
+              image: res.data.image,
+              name: res.data.name,
+              details: res.data.details,
+              price: res.data.price,
+            };
+            this.errmsg = "";
+          } else {
+            this.errmsg = "Oops, something went wrong!";
+          }
+        })
+        .catch(() => {
+          this.isLoading = false;
+          this.errmsg = "Oops, something went wrong!";
+        });
+    },
+  },
+  created() {
+    const id = this.$route.params.carId;
+    this.getCarData(id);
+  },
+};
+</script>

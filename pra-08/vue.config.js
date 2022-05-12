e3de2695ba8f8b9971c07fdbef1c24532ce7ef4c
Smit3112 <smit.t@simformsolutions.com>
2022-05-12 14:55:19 +0530

    added : proxy in vue.config file

--- pra-08/vue.config.js
+++ pra-08/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "^/api": {
+        target:
+          "http://www.mockbin.org/bin/acab9b21-f102-41a0-b68e-52070bff1563",
+        changeOrigin: true,
+      },
+    },
+  },
+});

cb9e350401cdbb822fd497857c99aad3f2a8f47e
Smit3112 <smit.t@simformsolutions.com>
2022-05-16 15:06:57 +0530

    initial commit & addded mapgatters

---
+++ pra-09/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <transition name="bounce">
+      <router-view apper :key="$router.path" />
+    </transition>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/Navbar.vue";
+export default {
+  name: "App",
+  components: { NavBar },
+  mounted() {
+    if (this.$cookies.get("authUser")) {
+      this.$store.commit("auth/isAuth", true);
+    } else {
+      this.$store.commit("auth/isAuth", false);
+    }
+  },
+};
+</script>
+
+<style>
+@import "vue2-animate/dist/vue2-animate.min.css";
+</style>
+
+
+

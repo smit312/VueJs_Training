f11300836478bfa20a928b09d5a722ec712a4ede
Smit3112 <smit.t@simformsolutions.com>
2022-07-07 12:13:14 +0530

    pra-11

---
+++ pra-11/src/components/Navbar.vue
@@ -0,0 +1,48 @@
+ <template>
+  <div>
+    <v-app-bar color="deep-black accent-4" dense dark min-width="300">
+      <v-toolbar-title>{{ showRoomName }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn plain v-if="this.isAuth" @click="openModal">Add Car</v-btn>
+      <v-btn plain v-if="this.isAuth" to="/UsersList">Uers</v-btn>
+      <v-btn plain v-if="this.isAuth" to="/">Home</v-btn>
+      <v-btn plain v-if="!this.isAuth" to="/login"> Login</v-btn>
+      <v-btn plain v-if="!this.isAuth" to="/register"> Register</v-btn>
+      <v-btn plain v-if="this.isAuth" @click.prevent="logoutHandler">
+        Logout</v-btn
+      >
+    </v-app-bar>
+  </div>
+</template> 
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      showRoomName: "Car Showroom",
+    };
+  },
+  computed: {
+    ...mapGetters(["auth/getAllUser"]),
+    isAuth() {
+      return this.$store.getters["auth/getisAuth"];
+    },
+    userData() {
+      return this.$store.getters["auth/getUser"];
+    },
+  },
+  methods: {
+    openModal() {
+      this.$store.commit("ui/openDialog", { type: "add" });
+    },
+    logoutHandler() {
+      this.$store.dispatch("auth/UserLogout");
+    },
+  },
+};
+</script>
+
---
+++ pra-11/src/views/Users.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="this.AllUser"
+      :items-per-page="10"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+ 
+
+<script>
+export default {
+  name: "UsersList",
+  data() {
+    return {
+      headers: [
+        { text: "id", value: "id" },
+        {
+          text: "Name",
+          align: "start",
+          sortable: true,
+          value: "name",
+        },
+        { text: "Email", value: "email" },
+        { text: "role", value: "role" },
+        { text: "age", value: "age" },
+        { text: "DOB", value: "dob" },
+        { text: "gender", value: "gender" },
+      ],
+    };
+  },
+  computed: {
+    AllUser() {
+      return this.$store.getters["auth/getAllUser"];
+    },
+  },
+};
+</script>
